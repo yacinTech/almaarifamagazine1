acbcd5db4e5e68683aefd77c35f1c4294a033b38
yacinTech <jeenmatheo2@gmail.com>
2025-05-28 23:12:00 +0100

    done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,26 @@
-// pages/_app.tsx
-import { AppProps } from 'next/app';
-import Script from 'next/script';
+import { useEffect } from "react";
+import Script from "next/script";
+import type { AppProps } from "next/app";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.dataLayer = window.dataLayer || [];
+      function gtag(...args: any[]) {
+        window.dataLayer.push(args);
+      }
+      gtag("js", new Date());
+      gtag("config", "G-BG1PE488V6");
+    }
+  }, []);
+
   return (
     <>
-      {/* Google Tag Manager */}
+      {/* Google tag (gtag.js) */}
       <Script
-        strategy="afterInteractive"
+        async
         src="https://www.googletagmanager.com/gtag/js?id=G-BG1PE488V6"
-      />
-      <Script
-        id="gtag-init"
         strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-BG1PE488V6');
-          `,
-        }}
       />
       <Component {...pageProps} />
     </>

dce01dc9a6707d7a5de46b2afb8ccd612e8d752f
yacinTech <jeenmatheo2@gmail.com>
2025-05-30 19:41:00 +0100

    done

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://almaarifamagazine.vercel.app',
+  generateRobotsTxt: true, // توليد ملف robots.txt تلقائيًا
+};
